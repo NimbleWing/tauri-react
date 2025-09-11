3cf79409b67691629ed27bd0edd5001180c71785
Pure <305737824@qq.com>
2025-09-11 18:53:44 +0800

    chore: Add prettier config and integrate with eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,11 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
   pluginReact.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
 ]);
